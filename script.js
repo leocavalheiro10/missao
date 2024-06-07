5c7bd8a1f750ebc9531f03e05355ea51b28d7b8f
leocavalheiro10 <105437927+leocavalheiro10@users.noreply.github.com>
2024-06-07 08:48:45 -0300

    ok

---
+++ script.js
@@ -0,0 +1,13 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPergunta = document.querySelector(".caixa-pergunta");
+const caixaAlternativa = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+const perguntas = [
+    {
+
+},
+    {
+    
+}
+]
